8ac009a53ef2dfbb8f655d69aff6f6770a8a9e99
Nahid Akbar <nahid.akbar@data61.csiro.au>
2019-06-05 09:33:25 +1000

    extra metadata from linkedin

--- src/Data/summary.ts
+++ src/Data/summary.ts
@@ -17,3 +17,18 @@ const SummaryData = [
 ];
 
 export default SummaryData;
+
+/**
+
+I have:
+
+- A can do attitude and experience in full SDLC roles. 
+- Strong ability to adapt technologically and research skills  gained through over ten years of project experience and university study. I enjoy learning new subjects and ideas to solve my problems in both efficient and effective ways.
+- Highly developed organisational and time management skills – able to plan, manage and juggle multiple responsibilities to achieve objectives.
+- Strong leadership capabilities and is a supportive team player. Enjoy taking initiatives to achieve set goals and can work independently with minimal supervision.
+- Excellent communication and interpersonal skills with experience relating to technical and non-technical people on all levels and work in multi-disciplinary project teams.
+- Advanced computing and written communication skills with experience in writing different types of technical documentation and consulting with stakeholders.
+- Strong interest and involvement in professional development activities.
+
+I am a polyglot. I have general preference for flatter organisations, cultures and teams - the type of environment where people manage themselves and take initiatives to do amazing things. I also like working with people who like taking ownership of projects as a whole (not their small part), who prefer to step outside the box by default and can appreciate the following inequality: software engineering >> coding + testing.
+*/
